434e258eeeb3f3a1c46092f974a3eabe1cfed107
annabook21 <asiabooker21@gmail.com>
2025-05-31 13:12:23 -0400

    module 2

---
+++ module2/src/main/java/edu/au/cpsc/module2/SeatReservation.java
@@ -0,0 +1,60 @@
+package edu.au.cpsc.module2;
+
+import java.time.LocalDate;
+
+public class SeatReservation {
+    private String flightDesignator;
+    private LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+    
+    public SeatReservation() {
+    }
+    
+    public SeatReservation(String flightDesignator, LocalDate flightDate, String firstName, String lastName) {
+        setFlightDesignator(flightDesignator);
+        this.flightDate = flightDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+    
+    public void setFlightDesignator(String flightDesignator) {
+        if (flightDesignator == null || flightDesignator.length() < 4 || flightDesignator.length() > 6) {
+            throw new IllegalArgumentException();
+        }
+        this.flightDesignator = flightDesignator;
+    }
+    
+    public LocalDate getFlightDate() {
+        return flightDate;
+    }
+    
+    public void setFlightDate(LocalDate flightDate) {
+        this.flightDate = flightDate;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String toString() {
+        return "SeatReservation{flightDesignator=" + flightDesignator + ",flightDate=" + flightDate + ",firstName=" + firstName + ",lastName=" + lastName + "}";
+    }
+}
+
