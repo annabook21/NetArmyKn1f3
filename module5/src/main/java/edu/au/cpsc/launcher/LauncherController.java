f2ded4c559baf0a58a540d98dbce2cb98acd8732
annabook21 <asiabooker21@gmail.com>
2025-07-13 06:43:28 -0400

    Clean repository structure - keep only NetArmyKn1f3 application
    - Remove all module1-6 directories and legacy files
    - Move module7 content to root level for clean structure
    - Remove temporary files and IDE configurations
    - Keep only essential NetArmyKn1f3 application files

--- module5/src/main/java/edu/au/cpsc/launcher/LauncherController.java
+++
@@ -1,5 +0,0 @@
-package edu.au.cpsc.launcher;
-
-public class LauncherController {
-    // Empty class as required
-}
--- module5/src/main/java/edu/au/cpsc/miscstyle/Part1Controller.java
+++
@@ -1,8 +0,0 @@
-package edu.au.cpsc.miscstyle;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class Part1Controller {
-
-}
